896d6ff4c71c454fa310727c6e5062b77325d244
DeTTK <ignatovichdmitrij096@gmail.com>
2026-04-19 22:27:31 +0300

    chore(config): remove allowedDevOrigins override

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 const nextConfig: NextConfig = {
-	allowedDevOrigins: ['37.45.63.217', 'http://37.45.63.217'],
 	images: {
 		remotePatterns: [
 			{
