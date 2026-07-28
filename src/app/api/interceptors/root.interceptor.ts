d6d7792e12ab7f76b3da42a499bc3ec3536783ee
oarer <oarer.yml@gmail.com>
2026-07-29 01:38:03 +0500

    fix root interceptor

--- src/app/api/interceptors/root.interceptor.ts
+++ src/app/api/interceptors/root.interceptor.ts
@@ -4,17 +4,20 @@ import { useBanStore } from '@/stores/useBan.store'
 export const apiClient = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API,
 	timeout: 10_000,
-	headers: { 'Content-Type': 'application/json' },
+	headers: {
+		'Content-Type': 'application/json',
+	},
 	withCredentials: true,
 })
 
 let isRefreshing = false
+
 let failedQueue: Array<{
-	resolve: (value?: unknown) => void
+	resolve: () => void
 	reject: (reason?: unknown) => void
 }> = []
 
-const processQueue = (error: unknown) => {
+const processQueue = (error?: unknown) => {
 	failedQueue.forEach(({ resolve, reject }) => {
 		if (error) {
 			reject(error)
@@ -22,11 +25,13 @@ const processQueue = (error: unknown) => {
 			resolve()
 		}
 	})
+
 	failedQueue = []
 }
 
 apiClient.interceptors.response.use(
 	(response) => response,
+
 	async (error) => {
 		const originalRequest = error.config
 
@@ -41,34 +46,43 @@ apiClient.interceptors.response.use(
 					error.response?.data?.reason,
 					error.response?.data?.expire_in
 				)
+
 			return Promise.reject(error)
 		}
 
-		if (error.response?.status === 401 && !originalRequest._retry) {
-			if (isRefreshing) {
-				return new Promise((resolve, reject) => {
-					failedQueue.push({ resolve, reject })
-				})
-					.then(() => apiClient(originalRequest))
-					.catch((err) => Promise.reject(err))
-			}
-
-			originalRequest._retry = true
-			isRefreshing = true
-
-			try {
-				await apiClient.post('/api/v1/auth/refresh')
-				processQueue(null)
-				return apiClient(originalRequest)
-			} catch (refreshError) {
-				processQueue(refreshError)
-				window.location.href = '/auth'
-				return Promise.reject(refreshError)
-			} finally {
-				isRefreshing = false
-			}
+		if (error.response?.status !== 401) {
+			return Promise.reject(error)
 		}
 
-		return Promise.reject(error)
+		if (originalRequest._retry) {
+			return Promise.reject(error)
+		}
+
+		if (isRefreshing) {
+			return new Promise<void>((resolve, reject) => {
+				failedQueue.push({
+					resolve,
+					reject,
+				})
+			}).then(() => {
+				return apiClient(originalRequest)
+			})
+		}
+
+		originalRequest._retry = true
+		isRefreshing = true
+
+		try {
+			await apiClient.post('/api/v1/auth/refresh')
+			processQueue()
+
+			return apiClient(originalRequest)
+		} catch (refreshError) {
+			processQueue(refreshError)
+
+			return Promise.reject(refreshError)
+		} finally {
+			isRefreshing = false
+		}
 	}
 )
