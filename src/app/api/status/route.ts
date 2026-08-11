d63f30fb0ef03f4a54db7e1f55c86715ac9db78d
oarer <oarer.yml@gmail.com>
2026-08-11 22:03:24 +0500

    fix: api status route

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -6,7 +6,7 @@ import type { StatusResponse } from '@/types/status.type'
 export async function GET(request: Request) {
 	try {
 		const { data } = await axios.get<StatusResponse>(
-			`https://status.stalhub.tech/dashboard-apis/monitor-bars?tags=stalhub,stalhub_cdn&days=1`,
+			`https://status.stalhub.dev/dashboard-apis/monitor-bars?tags=stalhub,stalhub_cdn&days=1`,
 			{
 				headers: {
 					Authorization: `Bearer ${process.env.STATUS_TOKEN}`,
