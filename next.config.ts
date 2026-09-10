afa4b7356838e0ada08f504e305b3e88cf070ff6
oarer <oarer.yml@gmail.com>
2026-09-10 14:35:55 +0500

    update static route

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 const nextConfig: NextConfig = {
+	assetPrefix: process.env.NEXT_PUBLIC_CDN_URL || undefined,
 	output: 'standalone',
 	allowedDevOrigins: ['192.168.1.40', 'localhost'],
 	images: {
